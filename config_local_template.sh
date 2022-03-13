f207b1acf33c79afb097bcf2d159be419ef131a5
mshumilova <99672002+mshumilova@users.noreply.github.com>
2022-03-14 02:28:00 +0300

    template

---
+++ config_local_template.sh
@@ -0,0 +1,14 @@
+##__________________________________________________________________________________________________________
+##### local configurations
+##__________________________________________________________________________________________________________
+
+readonly path=""                          #path to your working directory
+readonly varscan=""                       #path to VarScan (.jar)
+ 
+readonly input_dir=$path/input_dir        #do not change. the path to directory with initial data
+readonly interm_dir=$path/interm_dir      #do not change. the path to directory with intermediate data
+readonly output_dir=$path/output_dir      #do not change. the path to directory with results
+
+readonly ref_dir=$input_dir/ref_dir       #do not change. the path to directory with the reference 
+readonly samp_dir=$input_dir/samp_dir     #do not change. the path to directory with the sample data
+readonly cont_dir=$input_dir/controls     #do not change. the path to directory with the controls
